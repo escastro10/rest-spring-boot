f585d2179d1e34c907988bb391bd15fe1370d5ed
Ebert Santana Castro <ebert.castro@gruporecursos.intranet>
2023-06-14 10:59:20 -0300

    atualização do projeto notebook novo

---
+++ 05_PrimeirosPassosSpringBoot/rest-spring-boot/src/test/java/br/com/boot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# rest-spring-boot
+# rest-with-spring-boot-and-java
+Construindo uma API REST com spring boot.
